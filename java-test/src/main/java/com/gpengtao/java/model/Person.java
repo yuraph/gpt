a4d5293215cb6daacc45205b2fe4c3cd5d814682
耿鹏涛 <pengtao.geng@wormpex.com>
2018-05-17 20:06:19 +0800

    修改

---
+++ java-test/src/main/java/com/gpengtao/java/model/Person.java
@@ -0,0 +1,50 @@
+package com.gpengtao.java.model;
+
+/**
+ * Created by pengtao.geng on 2017/11/2.
+ */
+public class Person {
+    private String name;
+    private String city;
+    private int age;
+
+    public Person() {
+    }
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", city='" + city + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
