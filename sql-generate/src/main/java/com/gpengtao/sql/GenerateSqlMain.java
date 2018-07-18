ac7ee85cca9fd92091e51d9e4898d6662e687cc4
耿鹏涛 <pengtao.geng@wormpex.com>
2018-07-18 22:11:13 +0800

    xiugai

--- sql-generate/src/main/java/com/gpengtao/sql/GenerateSqlMain.java
+++ sql-generate/src/main/java/com/gpengtao/sql/GenerateSqlMain.java
@@ -26,7 +26,7 @@ public class GenerateSqlMain {
 
     public static void main(String[] args) throws SQLException, IOException {
 
-        String url = "jdbc:mysql://common1-w-mysql.dev.wormpex.com:33006/idss_ims_productplan?useUnicode=true&amp;characterEncoding=UTF-8";
+        String url = "jdbc:mysql://common1-w-mysql.dev..com:33006/idss_ims_productplan?useUnicode=true&amp;characterEncoding=UTF-8";
         String username = "dev";
         String password = "096667f9c7d0396d";
         String tableName = "*";
